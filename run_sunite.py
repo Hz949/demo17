5168d2099d11ffbceb7c6c118f5c0ddbcf16173b
Hz949 <hz49188@163.com>
2019-10-17 16:05:11 +0800

    add report

--- run_sunite.py
+++ run_sunite.py
@@ -1,9 +1,10 @@
 import unittest
 
-
 # 组织测试套件对象
+import app
 from case.TestEmployee import TestEmployee
 from case.TestIHRMUser import TestUser
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 suite.addTest(TestUser("test_login_success"))
@@ -12,5 +13,6 @@ suite.addTest(TestEmployee("test_emp_update"))
 suite.addTest(TestEmployee("test_emp_get"))
 suite.addTest(TestEmployee("test_emp_delete"))
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
+with open(app.PRO_PATH + "/report/rep.html", "wb")as f:
+    runner = HTMLTestRunner(f, title="hello", description="v1.0")
+    runner.run(suite)
